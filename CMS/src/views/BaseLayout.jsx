978fb690aabd2db978e917165cc5d1b0e7011519
AjiNotoKusumo <aji201101@gmail.com>
2026-02-15 23:07:51 +0700

    Update notification

--- CMS/src/views/BaseLayout.jsx
+++ CMS/src/views/BaseLayout.jsx
@@ -1,11 +1,14 @@
 import { Navigate, Outlet, useNavigate } from "react-router";
 import Navbar from "../components/Navbar"
 import Sidebar from "../components/Sidebar"
+import notification from "../helpers/notification";
 
 
 export default function BaseLayout() {
 
     if(!localStorage.access_token) {
+        notification(`Please login first`, 'error')
+
         return (
             <>
                 <Navigate to="/login" />
